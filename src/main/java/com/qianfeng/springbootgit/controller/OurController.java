4a569be6796d4945e15c603d0a8c5fa3ba4a9de2
husong <331545547@qq.com>
2019-06-05 19:57:02 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/springbootgit/controller/OurController.java
@@ -0,0 +1,13 @@
+package com.qianfeng.springbootgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author husong
+ * @Date 2019/6/5
+ */
+@Controller
+@RequestMapping("/our")
+public class OurController {
+}
